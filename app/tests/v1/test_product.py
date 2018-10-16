05ce22ac9dd9cca1b57e1a3845214a34ba6ec460
Olal Keith <jkeneth.jk@gmail.com>
2018-10-16 15:56:10 +0300

    [Chore #161214224] test to delete a product

--- app/tests/v1/test_product.py
+++ app/tests/v1/test_product.py
@@ -23,3 +23,12 @@ class ProductTestCase(unittest.TestCase):
                                                   price='price')),
                                          content_type='application/json')
         self.assertEqual(add_product.status_code, 201)
+
+    def test_to_delete_product(self):
+        """Test method to delete a product"""
+        delete_product = self.client().delete('/api/v1/product/pencil',
+                                              data=json.dumps(
+                                                  dict(category='catgegory',
+                                                       price='price')),
+                                              content_type='application/json')
+        self.assertEqual(delete_product.status_code, 200)
