b62091d73eb231e0a5b8f4fc74adb121a6acb670
Olal Keith <jkeneth.jk@gmail.com>
2018-10-18 21:59:54 +0300

    [Feature #161248671] add an option to delete a product using id

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,9 @@ def create_app(config_name='testing'):
     app.config.from_object(app_config['testing'])
 
     from .api.v1.views import Product
+    from .api.v1.views import ProductId
 
     api.add_resource(Product, '/api/v1/product/<string:name>')
+    api.add_resource(ProductId, '/api/v1/product/<int:id>')
 
     return app
--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -8,7 +8,7 @@ products = []  # using in memory , just a python list
 
 
 class Product(Resource):
-    """Class to handle post and get for product"""
+    """Class to handle post for product"""
 
     def post(self, name):
         """Method to add/create a new product"""
@@ -20,21 +20,27 @@ class Product(Resource):
             # getting the json payload from a request
 
         data = request.get_json()
-        product = {'id': len(products) + 1,
+        product = {'product_id': len(products) + 1,
                    'name': name,
                    'category': data['category'],
-                   'price': data['price']
+                   'price': data['price'],
+                   'Quantity': data['Quantity'],
+                   'Description': data['Description']
                    }
         products.append(product)
         return product, 201
 
-    def delete(self, name):
-        """Method to delete a single product"""
-        # the products variable in this block is the outer 'products =[]'
-        # variable
-        global products
-        # list after the result of filtering
-        # looking for all the elements except the one that is going to be
-        # delated
-        products = list(filter(lambda x: x['name'] != name, products))
-        return {'message': 'item deleted'}
+
+class ProductId(Resource):
+    """Class to handle the delete endpoint"""
+
+    def delete(self, id):
+            """Method to delete a single product"""
+            # the products variable in this block is the outer 'products =[]'
+            # variable
+            global products
+            # list after the result of filtering
+            # looking for all the elements except the one that is going to be
+            # delated
+            products = list(filter(lambda x: x['product_id'] != id, products))
+            return {'message': 'item deleted'}
