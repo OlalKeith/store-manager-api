ef285c1cfc1c9bba9472830ae8304b7cd0a9e636
Olal Keith <jkeneth.jk@gmail.com>
2018-10-18 22:13:53 +0300

    [Chore #161214224] update the TestCase for deleting a product

--- app/tests/v1/test_product.py
+++ app/tests/v1/test_product.py
@@ -9,26 +9,25 @@ class ProductTestCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app('testing')
         self.client = self.app.test_client
-        # self.my_product = {
-        #     'price': '50',
-        #     'name': 'pencil',
-        #     'category': 'stationery'
-        # }
 
     def test_to_add_product(self):
         """Test method to add product"""
         add_product = self.client().post('/api/v1/product/pencil',
                                          data=json.dumps(
                                              dict(category='category',
+                                                  Quantity='Quantity',
+                                                  Description='Description',
                                                   price='price')),
                                          content_type='application/json')
         self.assertEqual(add_product.status_code, 201)
 
     def test_to_delete_product(self):
         """Test method to delete a product"""
-        delete_product = self.client().delete('/api/v1/product/pencil',
+        delete_product = self.client().delete('/api/v1/product/1',
                                               data=json.dumps(
                                                   dict(category='catgegory',
+                                                       Quantity='Quantity',
+                                                       Description='Description',
                                                        price='price')),
                                               content_type='application/json')
         self.assertEqual(delete_product.status_code, 200)
