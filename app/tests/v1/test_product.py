292f8235f48dd94a5b1873834ff427b2403970b8
Olal Keith <jkeneth.jk@gmail.com>
2018-10-20 13:21:42 +0300

    [Chore #161214224] test an existing product exists

--- app/tests/v1/test_product.py
+++ app/tests/v1/test_product.py
@@ -20,3 +20,9 @@ class ProductTestCase(unittest.TestCase):
                                                   price='price')),
                                          content_type='application/json')
         self.assertEqual(add_product.status_code, 201)
+
+    def test_to_add_existing_product(self):
+        """Test method to test an existing product"""
+        same_product = self.client().post('/api/v1/product/pencil',
+                                          content_type='application/json')
+        self.assertEqual(same_product.status_code, 400)
