fdcaea172afa2d00953fee8be5834ff0ec258184
Olal Keith <jkeneth.jk@gmail.com>
2018-10-25 23:22:01 +0300

    [Chore #161214224] update TestCases

--- app/tests/v1/test_product.py
+++ app/tests/v1/test_product.py
@@ -2,38 +2,34 @@ import unittest
 import json
 from app import create_app
 
+base_url = 'api/v1'
 
-class ProductTestCase(unittest.TestCase):
-    """This is the class for product test cases"""
+
+class Test_Product_Case(unittest.TestCase):
+    """Class for product test cases"""
 
     def setUp(self):
         self.app = create_app('testing')
-        self.client = self.app.test_client
-        # self.my_product = {
-        #     'price': '50',
-        #     'name': 'pencil',
-        #     'category': 'stationery'
-        # }
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.products = {
+            'category': 'stationery',
+            'product_id': 1,
+            'quantity': 5,
+            'description': 'I use to write with it',
+            'price': 30.00
+        }
 
-    def test_to_add_product(self):
+    def test_to_create_product(self):
         """Test method to add product"""
-        add_product = self.client().post('/api/v1/product/pencil',
-                                         data=json.dumps(
-                                             dict(category='category',
-                                                  Quantity='Quantity',
-                                                  Description='Description',
-                                                  price='price')),
-                                         content_type='application/json')
-        self.assertEqual(add_product.status_code, 201)
+
+        create_product = self.client.post(base_url + '/product/pencil', data=json.dumps(
+            self.products), content_type='application/json')
+        self.assertEqual(create_product.status_code, 201)
 
     def test_to_get_all_products(self):
         """Test method to get all products"""
-        get_all_products = self.client().get('/api/v1/products',
-                                             data=json.dumps(
-                                                 dict(
-                                                     category='category',
-                                                     Quantity='Quantity',
-                                                     Description='Description',
-                                                     price='price')),
-                                             content_type='application/json')
+        get_all_products = self.client.get(base_url + '/products',
+                                           data=json.dumps(self.products),
+                                           content_type='application/json')
         self.assertEqual(get_all_products.status_code, 200)
