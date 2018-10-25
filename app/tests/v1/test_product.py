75f19ffd16a6570754b1db4112d11ec3b98cc331
Olal Keith <jkeneth.jk@gmail.com>
2018-10-25 14:00:48 +0300

    [Chore #161214224] modify the Test Cases

--- app/tests/v1/test_product.py
+++ app/tests/v1/test_product.py
@@ -2,32 +2,34 @@ import unittest
 import json
 from app import create_app
 
+base_url = 'api/v1'
 
-class ProductTestCase(unittest.TestCase):
-    """This is the class for product test cases"""
+
+class Test_Product_Case(unittest.TestCase):
+    """Class for product test cases"""
 
     def setUp(self):
         self.app = create_app('testing')
-        self.client = self.app.test_client
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.products = {
+            'Category': 'stationery',
+            'product_id': 1,
+            'Quantity': 5,
+            'Description': 'I use to write with it',
+            'price': 30.00
+        }
 
-    def test_to_add_product(self):
+    def test_to_create_product(self):
         """Test method to add product"""
-        add_product = self.client().post('/api/v1/product/pencil',
-                                         data=json.dumps(
-                                             dict(category='category',
-                                                  Quantity='Quantity',
-                                                  Description='Description',
-                                                  price='price')),
-                                         content_type='application/json')
-        self.assertEqual(add_product.status_code, 201)
+
+        create_product = self.client.post(base_url + '/product/pencil', data=json.dumps(
+            self.products), content_type='application/json')
+        self.assertEqual(create_product.status_code, 201)
 
     def test_to_delete_product(self):
         """Test method to delete a product"""
-        delete_product = self.client().delete('/api/v1/product/1',
-                                              data=json.dumps(
-                                                  dict(category='catgegory',
-                                                       Quantity='Quantity',
-                                                       Description='Description',
-                                                       price='price')),
-                                              content_type='application/json')
+        delete_product = self.client.delete(base_url + '/product/1',
+                                            data=json.dumps(self.products),
+                                            content_type='application/json')
         self.assertEqual(delete_product.status_code, 200)
