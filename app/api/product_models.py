83594424fff66dd8145b939846097bdd7316dc40
Olal Keith <jkeneth.jk@gmail.com>
2018-10-25 09:23:31 +0300

    [Chore #161471470] refactor code to implement oop

---
+++ app/api/product_models.py
@@ -0,0 +1,28 @@
+products_item = []
+sales = []
+
+
+class ProductModel():
+    """docstring for  Product"""
+
+    def __init__(self, name, price, product_id, Category, Quantity, Description):
+
+        self.name = name
+        self.price = price
+        self.Category = Category
+        self.Quantity = Quantity
+        self.Description = Description
+        self.product_id = product_id
+
+    def add_product(self):
+        product = {
+
+            'name': self.name,
+            'Category': self.Category,
+            'price': self.price,
+            'Quantity': self.Quantity,
+            'Description': self.Description,
+            'product_id': self.product_id
+        }
+
+        products_item.append(product)
