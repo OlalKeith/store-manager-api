4cad30ab6ae40e89ef927cefd334ac30ad3d47dd
Olal Keith <jkeneth.jk@gmail.com>
2018-10-26 05:16:15 +0300

    [Chore #161471470] work on OOP

--- app/__init__.py
+++ app/__init__.py
@@ -11,10 +11,10 @@ def create_app(config_name='testing'):
 
     app.config.from_object(app_config['testing'])
 
-    from .api.v1.views import Product
-    from .api.v1.sales import Sales
+    from .api.v1.views import ProductView
+    from .api.v1.sales import SalesView
 
-    api.add_resource(Product, '/api/v1/product/<string:name>')
-    api.add_resource(Sales, '/api/v1/sale/<string:name>')
+    api.add_resource(ProductView, '/api/v1/product/<string:name>')
+    api.add_resource(SalesView, '/api/v1/sale/<string:name>')
 
     return app
